2219964c4292e6168515f2adb749a4760412087c
sarachen19 <49016705+sarachen19@users.noreply.github.com>
2020-06-16 15:33:41 -0400

    test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import AllGroups from "./components/allGroups";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
 
+
 ReactDOM.render(
   <DndProvider backend={HTML5Backend}>
     <AllGroups />
